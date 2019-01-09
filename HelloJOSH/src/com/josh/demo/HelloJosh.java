eac1812a45a43737d0fcaca47151d36b62f1c562
lihaotian <lihaotian@joshvm.com>
2019-01-09 11:33:46 +0800

    add first demo HellJosh

---
+++ HelloJOSH/src/com/josh/demo/HelloJosh.java
@@ -0,0 +1,18 @@
+package com.josh.demo;
+
+/**
+ * Hello JOSH
+ * <P>
+ * Josh Studio 基础使用。
+ * 
+ * @author LHT
+ *
+ */
+public class HelloJosh {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello JOSH.");
+	}
+
+}
