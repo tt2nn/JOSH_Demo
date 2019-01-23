84efbc518db9d5a2f0d40a3f0d0300878f691a8f
lihaotian <lihaotian@joshvm.com>
2019-01-23 17:10:23 +0800

    添加SocketDemo

---
+++ SocketDemo/src/com/josh/demo/SocketDemo.java
@@ -0,0 +1,19 @@
+package com.josh.demo;
+
+/**
+ * Demo For Socket
+ * 
+ * @author josh
+ *
+ */
+public class SocketDemo {
+
+	// IP
+	private static String ip = "";
+	// 端口
+	private static String port = "";
+
+	public static void main(String[] args) {
+
+	}
+}
